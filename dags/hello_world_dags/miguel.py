46f59348e6e10197a8d2d975b96a622c12c60aec
magmogm <megilmaged198@gmail.com>
2025-12-04 15:49:43 +0200

    miguels2

--- dags/hello_world_dags/miguel.py
+++ dags/hello_world_dags/miguel.py
@@ -2,11 +2,11 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def hello_world():
-    print("Hello World from Airflow ✅")
+def hello_world_miguel():
+    print("Hello World from miguel ✅")
 
 with DAG(
-    dag_id="hello_world_dag",
+    dag_id="hello_world_miguel",
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,   # Run manually
     catchup=False,
